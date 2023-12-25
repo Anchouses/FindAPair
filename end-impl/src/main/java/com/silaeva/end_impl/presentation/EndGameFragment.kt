f5ee442125485aec55a1a0d073a57502532dcffd
Anchous <foranngel@gmail.com>
2023-12-25 15:04:56 +0300

    menu screen

--- end-impl/src/main/java/com/silaeva/end_impl/presentation/EndGameFragment.kt
+++ end-impl/src/main/java/com/silaeva/end_impl/presentation/EndGameFragment.kt
@@ -1,16 +1,13 @@
 package com.silaeva.end_impl.presentation
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.material3.MaterialTheme
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import com.silaeva.end_impl.R
 import com.silaeva.end_impl.databinding.FragmentEndGameBinding
-import com.silaeva.end_impl.navigation.EndGameScreens
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
